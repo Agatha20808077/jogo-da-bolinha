421d071b06746946033d030fb05fa1dfb0a3dff4
Agatha20808077 <96261296+Agatha20808077@users.noreply.github.com>
2022-12-17 11:06:12 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,38 @@
 var bolinha;
+var bolinhaImg;
+var obstaculo1;
+var edges;
+
+function preload(){
+  bolinhaImg = loadImage("bolinha.png");
+
+}
+
 
 function setup() {
-  createCanvas(700, 7000);
+  createCanvas(700, 700);
   bolinha = createSprite(350, 500 ); 
+  bolinha.addImage("bola", bolinhaImg);
+  bolinha.scale = 0.2;
+
+  obstaculo1 = createSprite(350, 100, 500, 50);
+  obstaculo1.velocityX = 20;
+  edges = createEdgeSprites();
 }
 
 function draw() {
   background("black");
 
+  obstaculo1.bounceOff(edges);
+
  
   if(keyDown("space")) {
-    bolinha.velocityY = -1.5;
+    bolinha.velocityY = -10;
   }
   
-  bolinha.velocityY += 0.1;
+  bolinha.velocityY += 1;
   
-  camera.y = bolinha.y;
+  //camera.y = bolinha.y;
 
   drawSprites();
 }
